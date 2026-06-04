518052f1bbdaef7004a2de897adc46e8b5e38887
ramakrishnabrk68 <ramakrishna.brk69@gmail.com>
2026-06-04 17:47:25 +0530

    added to image

--- force-app/main/default/lwc/cabbooKing/cabbooKing.js
+++ force-app/main/default/lwc/cabbooKing/cabbooKing.js
@@ -1,7 +1,17 @@
 import { LightningElement } from 'lwc';
 import Location from '@salesforce/resourceUrl/Location';
+ import CabBooking from '@salesforce/resourceUrl/CabBooking';
+ import Rescheduled from '@salesforce/resourceUrl/Rescheduled';
+ import carouselimage from '@salesforce/resourceUrl/carouselimage';
+
+
 import andhrapradesh from '@salesforce/resourceUrl/andhrapradesh';
 export default class CabbooKing extends LightningElement {
     Location = Location;
     andhrapradesh = andhrapradesh;
+    booKing = CabBooking;  
+     Rescheduled = Rescheduled; 
+     carouselimage =carouselimage;
+
+    
 }
